5c1aadf1e41e01b4ec4aadac913f5ac2cbef2503
mkideal <i@mkideal.com>
2022-02-16 16:34:14 +0800

    update event

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -25,6 +25,7 @@ type Scene struct {
 
 func NewScene() *Scene {
 	const aspect = 1
+	object.AddedEventType = nil
 	scene := new(Scene)
 	scene.SetBackground(core.Vec4(0.2, 0.3, 0.3, 1.0))
 	scene.camera = object.NewPerspectiveCamera(30, aspect, 0.1, 1000)
--- object/event.go
+++ object/event.go
@@ -7,14 +7,16 @@ type (
 	RemovedEvent struct{}
 )
 
+//@mod:final
 var (
 	AddedEventType   = event.TypeOf[*AddedEvent](nil)
-	RemovedEventType = event.TypeOf[*AddedEvent](nil)
+	RemovedEventType = event.TypeOf[*RemovedEvent](nil)
 )
 
 func (AddedEvent) Type() event.Type   { return AddedEventType }
 func (RemovedEvent) Type() event.Type { return RemovedEventType }
 
+//@mod:final
 var (
 	addedEvent   = AddedEvent{}
 	removedEvent = RemovedEvent{}
